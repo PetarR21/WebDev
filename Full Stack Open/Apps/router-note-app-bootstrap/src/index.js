e0e73cafbf43c99535dc75ec1b114558f044d727
PetarR21 <petar.r.rakocevic@gmail.com>
2022-09-22 18:00:15 +0200

    Finished part 7c

--- Full Stack Open/Apps/router-note-app-bootstrap/src/index.js
+++ Full Stack Open/Apps/router-note-app-bootstrap/src/index.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Form, Table, Button, Alert, Navbar, Nav } from 'react-bootstrap';
 import ReactDOM from 'react-dom/client';
 import {
   BrowserRouter as Router,
@@ -39,15 +40,19 @@ const Note = ({ note }) => {
 
 const Notes = ({ notes }) => (
   <div>
-    {' '}
-    <h2>Notes</h2>{' '}
-    <ul>
-      {notes.map((note) => (
-        <li key={note.id}>
-          <Link to={`/notes/${note.id}`}>{note.content}</Link>
-        </li>
-      ))}
-    </ul>
+    <h2>Notes</h2>
+    <Table striped>
+      <tbody>
+        {notes.map((note) => (
+          <tr key={note.id}>
+            <td>
+              <Link to={`/notes/${note.id}`}>{note.content}</Link>
+            </td>
+            <td>{note.user}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   </div>
 );
 
@@ -63,15 +68,17 @@ const Login = (props) => {
   return (
     <div>
       <h2>login</h2>
-      <form onSubmit={onSubmit}>
-        <div>
-          username: <input />
-        </div>
-        <div>
-          password: <input type='password' />
-        </div>
-        <button type='submit'>login</button>
-      </form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group>
+          <Form.Label>username:</Form.Label>
+          <Form.Control type='text' name='username' />
+          <Form.Label>password:</Form.Label>
+          <Form.Control type='password' />
+          <Button variant='primary' type='submit'>
+            login
+          </Button>
+        </Form.Group>
+      </Form>
     </div>
   );
 };
@@ -110,6 +117,7 @@ const App = () => {
   ]);
 
   const [user, setUser] = useState(null);
+  const [message, setMessage] = useState(null);
 
   const padding = {
     padding: 5,
@@ -117,31 +125,49 @@ const App = () => {
 
   const login = (user) => {
     setUser(user);
+    setMessage(`welcome ${user}`);
+    setTimeout(() => {
+      setMessage(null);
+    }, 10000);
   };
 
   const match = useMatch('/notes/:id');
   const note = match ? notes.find((note) => note.id === Number(match.params.id)) : null;
 
   return (
-    <div>
-      <div>
-        <Link style={padding} to='/'>
-          home
-        </Link>
-        <Link style={padding} to='/notes'>
-          notes
-        </Link>
-        <Link style={padding} to='/users'>
-          users
-        </Link>
-        {user ? (
-          <em>{user} logged in</em>
-        ) : (
-          <Link style={padding} to='/login'>
-            login
-          </Link>
-        )}
-      </div>
+    <div className='container'>
+      {message && <Alert variant='success'>{message}</Alert>}
+      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            <Nav.Link href='#' as='span'>
+              <Link style={padding} to='/'>
+                home
+              </Link>
+            </Nav.Link>
+            <Nav.Link href='#' as='span'>
+              <Link style={padding} to='/notes'>
+                notes
+              </Link>
+            </Nav.Link>
+            <Nav.Link href='#' as='span'>
+              <Link style={padding} to='/users'>
+                users
+              </Link>
+            </Nav.Link>
+            <Nav.Link href='#' as='span'>
+              {user ? (
+                <em style={padding}>{user} logged in</em>
+              ) : (
+                <Link style={padding} to='/login'>
+                  login
+                </Link>
+              )}
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
       <Routes>
         <Route path='/notes/:id' element={<Note note={note} />} />
