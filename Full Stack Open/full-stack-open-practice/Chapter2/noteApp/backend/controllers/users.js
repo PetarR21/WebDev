013ce189b60c5124b99c5eb9e0678b190800712b
PetarR21 <petar.r.rakocevic@gmail.com>
2022-11-04 17:11:00 +0100

    Finished part 4c

---
+++ Full Stack Open/full-stack-open-practice/Chapter2/noteApp/backend/controllers/users.js
@@ -0,0 +1,32 @@
+const usersRouter = require('express').Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('notes', { content: 1, date: 1 });
+  response.json(users);
+});
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body;
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    return response.status(400).json({
+      error: 'username must be unique',
+    });
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
