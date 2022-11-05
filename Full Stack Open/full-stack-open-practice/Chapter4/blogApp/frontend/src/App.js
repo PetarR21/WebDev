8991b37d3254194344874ccb697ad6f093c21201
PetarR21 <petar.r.rakocevic@gmail.com>
2022-11-05 18:04:06 +0100

    Finished exercise 5.6

--- Full Stack Open/full-stack-open-practice/Chapter4/blogApp/frontend/src/App.js
+++ Full Stack Open/full-stack-open-practice/Chapter4/blogApp/frontend/src/App.js
@@ -1,19 +1,17 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import Blog from './components/Blog';
+import BlogForm from './components/BlogForm';
+import LoginForm from './components/LoginForm';
 import Notification from './components/Notification';
+import Togglable from './components/Togglable';
 
 import blogService from './services/blogs';
 import loginService from './services/login';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [url, setUrl] = useState('');
   const [notification, setNotification] = useState(null);
 
   useEffect(() => {
@@ -29,14 +27,11 @@ const App = () => {
     }
   }, []);
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-
+  const handleLogin = async (credidentials) => {
     try {
-      const user = await loginService.login({ username, password });
+      const user = await loginService.login(credidentials);
       setUser(user);
-      setUsername('');
-      setPassword('');
+      
       blogService.setToken(user.token);
       window.localStorage.setItem('loggedUserJSON', JSON.stringify(user));
     } catch (error) {
@@ -49,92 +44,29 @@ const App = () => {
       <div>
         <h2>log in to application</h2>
         <Notification notification={notification} />
-        <form onSubmit={handleLogin}>
-          <div>
-            username{' '}
-            <input
-              type='text'
-              value={username}
-              onChange={({ target }) => {
-                setUsername(target.value);
-              }}
-            />
-          </div>
-          <div>
-            password{' '}
-            <input
-              type='password'
-              value={password}
-              onChange={({ target }) => {
-                setPassword(target.value);
-              }}
-            />
-          </div>
-          <button type='submit'>login</button>
-        </form>
+        <LoginForm handleLogin={handleLogin} />
       </div>
     );
   };
 
-  const addBlog = async (event) => {
-    event.preventDefault();
-
-    const blogObject = {
-      title,
-      author,
-      url,
-    };
-
+  const addBlog = async (blogObject) => {
     try {
       const savedBlog = await blogService.create(blogObject);
       setBlogs(blogs.concat(savedBlog));
-      setTitle('');
-      setAuthor('');
-      setPassword('');
       showNotification({ message: `a new blog ${savedBlog.title} by ${savedBlog.author} added`, type: 'success' });
+      blogFormRef.current.toggleVisibility();
     } catch (error) {
       showNotification({ message: error.response.data.error, type: 'error' });
     }
   };
 
+  const blogFormRef = useRef();
+
   const blogForm = () => {
     return (
-      <div>
-        <h2>create new</h2>
-        <form onSubmit={addBlog}>
-          <div>
-            title:
-            <input
-              type='text'
-              value={title}
-              onChange={({ target }) => {
-                setTitle(target.value);
-              }}
-            />
-          </div>
-          <div>
-            author:
-            <input
-              type='text'
-              value={author}
-              onChange={({ target }) => {
-                setAuthor(target.value);
-              }}
-            />
-          </div>
-          <div>
-            url:
-            <input
-              type='text'
-              value={url}
-              onChange={({ target }) => {
-                setUrl(target.value);
-              }}
-            />
-          </div>
-          <button type='submit'>create</button>
-        </form>
-      </div>
+      <Togglable buttonLabel='new blog' ref={blogFormRef}>
+        <BlogForm createBlog={addBlog} />
+      </Togglable>
     );
   };
 
