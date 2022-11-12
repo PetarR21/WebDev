1ff85da21f3a8913c55ca97897ae70f475c0e264
PetarR21 <petar.r.rakocevic@gmail.com>
2022-11-12 11:56:55 +0100

    Fisnished exercise 8.12

--- Full Stack Open/full-stack-open/part8/library/frontend/src/components/Authors.js
+++ Full Stack Open/full-stack-open/part8/library/frontend/src/components/Authors.js
@@ -1,8 +1,15 @@
-import { useQuery } from '@apollo/client';
-import { ALL_AUTHORS } from '../queries';
+import { useMutation, useQuery } from '@apollo/client';
+import { useState } from 'react';
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries';
+import Select from 'react-select';
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS);
+
+  const [born, setBorn] = useState('');
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, { refetchQueries: [{ query: ALL_AUTHORS }] });
 
   if (result.loading) {
     return null;
@@ -13,6 +20,21 @@ const Authors = (props) => {
   }
 
   const authors = result.data.allAuthors;
+
+  const options = authors.map((a) => {
+    return {
+      value: a.name,
+      label: a.name,
+    };
+  });
+
+  const update = (event) => {
+    event.preventDefault();
+
+    editAuthor({ variables: { name: selectedOption.value, setBornTo: Number(born) } });
+
+    setBorn('');
+  };
 
   return (
     <div>
@@ -33,6 +55,22 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <div>
+        <h2>Set birthyear</h2>
+        <form onSubmit={update}>
+          <Select defaultValue={selectedOption} onChange={setSelectedOption} options={options} />
+          <div>
+            born:{' '}
+            <input
+              value={born}
+              onChange={({ target }) => {
+                setBorn(target.value);
+              }}
+            />
+          </div>
+          <button type='submit'>update author</button>
+        </form>
+      </div>
     </div>
   );
 };
