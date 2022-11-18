5877fd39a6100484431fa2f22814568b1cce4f54
PetarR21 <petar.r.rakocevic@gmail.com>
2022-11-18 09:36:42 +0100

    Started exercise 8.17

--- Full Stack Open/full-stack-open/part8/phonebook-gql/frontend/src/App.js
+++ Full Stack Open/full-stack-open/part8/phonebook-gql/frontend/src/App.js
@@ -1,18 +1,34 @@
-import { useQuery } from '@apollo/client';
-import { useState } from 'react';
+import { useApolloClient, useQuery } from '@apollo/client';
+import { useEffect, useState } from 'react';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import PhoneForm from './components/PhoneForm';
+import LoginForm from './components/LoginForm';
 import { ALL_PERSONS } from './queries';
 
 const App = () => {
+  const [token, setToken] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
+
+  const client = useApolloClient();
 
   const result = useQuery(ALL_PERSONS);
 
+  useEffect(() => {
+    if (localStorage.getItem('phonenumbers-user-token')) {
+      setToken(localStorage.getItem('phonenumbers-user-token'));
+    }
+  }, []);
+
   if (result.loading) {
-    return <div>loading...</div>;
+    return <div>loading...</div>
   }
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   const notify = (message) => {
     setErrorMessage(message);
@@ -21,9 +37,20 @@ const App = () => {
     }, 10000);
   };
 
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    );
+  }
+
   return (
     <div>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
